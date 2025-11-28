5d596d98cbf2be569c78989eb0293d2e2756434d
Michael Leggo <mleggo1@hotmail.com>
2025-11-28 20:04:26 +0800

    Update base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Investment-Matchmaker/'
+  base: '/ultimate-target/',   // 👈 VERY IMPORTANT
 })
